7fd6dacc96f3872a76334d8d85bfe92fb73ae798
ShimaaShams2022 <110138416+ShimaaShams2022@users.noreply.github.com>
2024-11-29 21:48:11 +0200

    Update validate-branch-name.sh

--- .github/scripts/validate-branch-name.sh
+++ .github/scripts/validate-branch-name.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-# Get the branch name from the argument or the current branch if no argument is provided
+
 BRANCH_NAME=${1:-$(git rev-parse --abbrev-ref HEAD)}
 
-# The regex pattern
-PATTERN= '^(master|main|develop){1}$|^(feature|fix|hotfix|release/enhancement)\/.+$' 
+
+PATTERN='^(master|main|develop){1}$|^(feature|fix|hotfix|release|enhancement)/.+$'
+
 
 if [[ "$BRANCH_NAME" =~ $PATTERN ]]; then
-   echo "Branch name '$BRANCH_NAME' is valid."
-   exit 0 
+  echo "Branch name '$BRANCH_NAME' is valid."
+  exit 0 # نجاح
 else
-   echo "Branch name '$BRANCH_NAME' is invalid. It must match the pattern: $PATTERN"
-   exit 1 
+  echo "Branch name '$BRANCH_NAME' is invalid. It must match the pattern: $PATTERN"
+  exit 1 # فشل
 fi
