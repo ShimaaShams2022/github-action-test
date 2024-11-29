a8aef169b37a19611774b378236f77f684aa5201
ShimaaShams2022 <110138416+ShimaaShams2022@users.noreply.github.com>
2024-11-29 22:32:52 +0200

    Update validate-branch-name.sh

--- .github/scripts/validate-branch-name.sh
+++ .github/scripts/validate-branch-name.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-BRANCH_NAME=${1:-$(git rev-parse --abbrev-ref HEAD)} 
-# The regex pattern
-PATTERN= '^(master|main|develop){1}$|^(feature|fix|hotfix|release/enhancement)\/.+$' 
+# الحصول على اسم الفرع
+BRANCH_NAME=${1:-$(git rev-parse --abbrev-ref HEAD)}
 
-if [[ "$BRANCH_NAME" =~ $PATTERN ]]; then
-   echo "Branch name '$BRANCH_NAME' is valid."
-   exit 0 
+# النمط المطلوب لاسم الفرع
+PATTERN='^(bugfix|hotfix|release)/[A-Za-z0-9]+-[0-9]+-[A-Za-z0-9\-]+$'
+
+# التحقق من اسم الفرع باستخدام regex
+if echo "$BRANCH_NAME" | grep -Eq "$PATTERN"; then
+  echo "Branch name '$BRANCH_NAME' is valid."
+  exit 0 # نجاح
 else
-   echo "Branch name '$BRANCH_NAME' is invalid. It must match the pattern: $PATTERN"
-   exit 1 
+  echo "Branch name '$BRANCH_NAME' is invalid. It must match the pattern: $PATTERN"
+  exit 1 # فشل
 fi
